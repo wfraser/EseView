0bc6ba2c17b790e837c9e05860fa8a9b185d3fd2
William R. Fraser <wfraser@codewise.org>
2015-03-02 16:45:53 -0800

    refactor the column retriever methods to their own class.

---
+++ EseView/ColumnRetriever.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Isam.Esent.Interop;
+using Esent = Microsoft.Isam.Esent.Interop.Api;
+
+namespace EseView
+{
+    public delegate object ColumnRetriever(JET_SESID sesid, JET_TABLEID table, JET_COLUMNID column);
+
+    public static class ColumnRetrievers
+    {
+        private static Dictionary<Type, ColumnRetriever> s_fetchers = new Dictionary<Type, ColumnRetriever>
+        {
+            { typeof(string),   Esent.RetrieveColumnAsString },
+            { typeof(long?),    (s,t,c) => Esent.RetrieveColumnAsInt64(s,t,c) },
+            { typeof(ulong?),   (s,t,c) => Esent.RetrieveColumnAsUInt64(s,t,c) },
+            { typeof(int?),     (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c) },
+            { typeof(uint?),    (s,t,c) => Esent.RetrieveColumnAsUInt32(s,t,c) },
+            { typeof(short?),   (s,t,c) => Esent.RetrieveColumnAsInt16(s,t,c) },
+            { typeof(ushort?),  (s,t,c) => Esent.RetrieveColumnAsUInt16(s,t,c) },
+            { typeof(byte?),    (s,t,c) => Esent.RetrieveColumnAsByte(s,t,c) },
+            { typeof(bool?),    (s,t,c) => Esent.RetrieveColumnAsBoolean(s,t,c) },
+            { typeof(double?),  (s,t,c) => Esent.RetrieveColumnAsDouble(s,t,c) },
+            { typeof(float?),   (s,t,c) => Esent.RetrieveColumnAsFloat(s,t,c) },
+
+            { typeof(DateTime?), (s,t,c) => Esent.RetrieveColumnAsDateTime(s,t,c) },
+            { typeof(Guid?),    (s,t,c) => Esent.RetrieveColumnAsGuid(s,t,c) },
+            { typeof(byte[]),   Esent.RetrieveColumn },
+           
+        };
+        
+        public static ColumnRetriever ForType(Type t)
+        {
+            if (!s_fetchers.ContainsKey(t))
+            {
+                return (x, y, z) => "Error: unhandled type.";
+            }
+            
+            return s_fetchers[t];
+        }
+
+        public static T Retrieve<T>(JET_SESID sesid, JET_TABLEID table, JET_COLUMNID column)
+        {
+            return (T)ForType(typeof(T))(sesid, table, column);
+        }
+    }
+}
--- EseView/DBReader.cs
+++ EseView/DBReader.cs
@@ -2,6 +2,7 @@
 using Microsoft.Isam.Esent.Interop.Vista;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Esent = Microsoft.Isam.Esent.Interop.Api;
 
 namespace EseView
@@ -12,36 +13,36 @@ namespace EseView
         private Session m_sesid;
         private JET_DBID m_dbid;
         private string m_filename;
-        private Dictionary<string, List<ColSpec>> m_tableDefs;
-        private Dictionary<Tuple<string, string>, List<ColSpec>> m_indexDefs;
+        private Dictionary<string, IEnumerable<ColSpec>> m_tableDefs;
+        private Dictionary<Tuple<string, string>, IEnumerable<ColSpec>> m_indexDefs;
 
         struct ColSpec
         {
             public string Name;
             public Type Type;
-            public Func<JET_SESID, JET_TABLEID, JET_COLUMNID, object> Fetch;
+            public ColumnRetriever Retriever;
             public JET_COLUMNID ColumnId;
         }
 
         public DBReader(string filename)
         {
             m_filename = filename;
-            m_tableDefs = new Dictionary<string, List<ColSpec>>();
-            m_indexDefs = new Dictionary<Tuple<string, string>, List<ColSpec>>();
+            m_tableDefs = new Dictionary<string, IEnumerable<ColSpec>>();
+            m_indexDefs = new Dictionary<Tuple<string, string>, IEnumerable<ColSpec>>();
 
             int pageSize;
             Esent.JetGetDatabaseFileInfo(filename, out pageSize, JET_DbInfo.PageSize);
 
-            string dir = System.IO.Path.GetDirectoryName(filename) + "\\";
+            string dir = Path.GetDirectoryName(filename) + Path.DirectorySeparatorChar;
             Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.DatabasePageSize, pageSize, null);
             Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.LogFilePath, 0, dir);
             Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.SystemPath, 0, dir);
 
             // Put the temp DB in our working directory.
-            Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.TempPath, 0, System.IO.Directory.GetCurrentDirectory() + "\\");
+            Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.TempPath, 0, Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar);
 
             // Recovery is disabled so that we don't touch the database's log files.
-            Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.Recovery, 0, null); // reco
+            Esent.JetSetSystemParameter(JET_INSTANCE.Nil, JET_SESID.Nil, JET_param.Recovery, 0, null);
 
             m_jetInstance = new Instance("ESEVIEW");
             m_jetInstance.Init();
@@ -105,45 +106,38 @@ namespace EseView
                 var colspec = new ColSpec();
                 colspec.Name = column.Name;
                 colspec.ColumnId = column.Columnid;
+                colspec.Retriever = null;
 
                 switch (column.Coltyp)
                 {
                     case JET_coltyp.Text:
                     case JET_coltyp.LongText:
-                        colspec.Fetch = Esent.RetrieveColumnAsString;
                         colspec.Type = typeof(string);
                         break;
                     case JET_coltyp.Long:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsInt32(s, t, c);
                         colspec.Type = typeof(int?);
                         break;
                     case JET_coltyp.Short:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsInt16(s, t, c);
                         colspec.Type = typeof(short?);
                         break;
                     case JET_coltyp.UnsignedByte:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsByte(s, t, c);
                         colspec.Type = typeof(byte?);
                         break;
                     case JET_coltyp.Bit:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsBoolean(s, t, c);
                         colspec.Type = typeof(bool?);
                         break;
                     case JET_coltyp.DateTime:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsDateTime(s, t, c);
                         colspec.Type = typeof(DateTime?);
                         break;
                     case JET_coltyp.IEEEDouble:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsDouble(s, t, c);
                         colspec.Type = typeof(double?);
                         break;
                     case JET_coltyp.IEEESingle:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsFloat(s, t, c);
                         colspec.Type = typeof(float?);
                         break;
                     case JET_coltyp.Binary:
                     case JET_coltyp.LongBinary:
-                        colspec.Fetch = (s, t, c) =>
+                        colspec.Retriever = (s, t, c) =>
                             {
                                 byte[] value = Esent.RetrieveColumn(s, t, c);
                                 if (value != null)
@@ -154,25 +148,26 @@ namespace EseView
                         colspec.Type = typeof(string);
                         break;
                     case VistaColtyp.UnsignedLong:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsUInt32(s, t, c);
                         colspec.Type = typeof(UInt32?);
                         break;
                     case VistaColtyp.LongLong:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsInt64(s, t, c);
                         colspec.Type = typeof(Int64?);
                         break;
                     case VistaColtyp.GUID:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsGuid(s, t, c);
                         colspec.Type = typeof(Guid?);
                         break;
                     case VistaColtyp.UnsignedShort:
-                        colspec.Fetch = (s, t, c) => Esent.RetrieveColumnAsUInt16(s, t, c);
                         colspec.Type = typeof(UInt16?);
                         break;
                     default:
-                        colspec.Fetch = (s, t, c) => "ERROR: unhandled type " + Enum.GetName(typeof(JET_coltyp), column.Coltyp) + "(" + (int)column.Coltyp + ")";
+                        colspec.Retriever = (s, t, c) => "ERROR: unhandled type " + Enum.GetName(typeof(JET_coltyp), column.Coltyp) + "(" + (int)column.Coltyp + ")";
                         colspec.Type = typeof(string);
                         break;
+                }
+
+                if (colspec.Retriever == null)
+                {
+                    colspec.Retriever = ColumnRetrievers.ForType(colspec.Type);
                 }
 
                 tableDef.Add(colspec);
@@ -232,7 +227,7 @@ namespace EseView
 
                     foreach (var column in m_tableDefs[tableName])
                     {
-                        value = column.Fetch(m_sesid, table, column.ColumnId);
+                        value = column.Retriever(m_sesid, table, column.ColumnId);
                         values.Add(value);
                     }
 
@@ -254,7 +249,7 @@ namespace EseView
             }
         }
 
-        private List<ColSpec> GetIndexTableDef(string tableName, string indexName)
+        private IEnumerable<ColSpec> GetIndexTableDef(string tableName, string indexName)
         {
             JET_INDEXLIST indexList;
             Esent.JetGetIndexInfo(m_sesid, m_dbid, tableName, indexName, out indexList, JET_IdxInfo.List);
@@ -262,95 +257,86 @@ namespace EseView
             // Unfortunately, Esent.GetTableColumns doesn't work on the temporary table returned by
             // JetGetIndexInfo, but the JET_INDEXLIST and the documentation have all the
             // information we need.
-            var tableDef = new List<ColSpec> {
+            var tableDef = new ColSpec[] {
                 new ColSpec
                 {
                     Name = "Columns",
-                    Fetch = (s, t, c) => Esent.RetrieveColumnAsInt32(s, t, c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcColumn
                 },
                 new ColSpec
                 {
                     Name = "Entries",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcEntry
                 },
                 new ColSpec
                 {
                     Name = "UniqueKeys",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcKey
                 },
                 new ColSpec {
                     Name = "Coltyp",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcoltyp
                 },
                 new ColSpec {
                     Name = "ColumnId",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcolumnid
                 },
                 new ColSpec {
                     Name = "ColumnName",
-                    Fetch = Esent.RetrieveColumnAsString,
                     Type = typeof(string),
                     ColumnId = indexList.columnidcolumnname
                 },
                 new ColSpec {
                     Name = "CodePage",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt16(s,t,c),
                     Type = typeof(Int16?),
                     ColumnId = indexList.columnidCp
                 },
                 new ColSpec {
                     Name = "NumPages",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidcPage
                 },
                 new ColSpec {
                     Name = "grbitColumn",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),  // TODO: It would be cool to get the enum value names here
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidgrbitColumn
                 },
                 new ColSpec {
                     Name = "grbitIndex",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),  // TODO: It would be cool to get the enum value names here
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidgrbitIndex
                 },
                 new ColSpec {
                     Name = "iColumn",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidiColumn
                 },
                 new ColSpec {
                     Name = "IndexName",
-                    Fetch = Esent.RetrieveColumnAsString,
                     Type = typeof(string),
                     ColumnId = indexList.columnidindexname
                 },
                 new ColSpec {
                     Name = "LangId",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt16(s,t,c),
                     Type = typeof(Int16?),
                     ColumnId = indexList.columnidLangid
                 },
                 new ColSpec {
                     Name = "LCMapFlags",
-                    Fetch = (s,t,c) => Esent.RetrieveColumnAsInt32(s,t,c),
                     Type = typeof(Int32?),
                     ColumnId = indexList.columnidLCMapFlags
                 }
             };
+
+            for (int i = 0, n = tableDef.Length; i < n; i++)
+            {
+                tableDef[i].Retriever = ColumnRetrievers.ForType(tableDef[i].Type);
+            }
 
             Esent.JetCloseTable(m_sesid, indexList.tableid);
 
@@ -363,7 +349,7 @@ namespace EseView
 
             if (!m_indexDefs.ContainsKey(new Tuple<string, string>(tableName, indexName)))
             {
-                List<ColSpec> tableDef = GetIndexTableDef(tableName, indexName);
+                IEnumerable<ColSpec> tableDef = GetIndexTableDef(tableName, indexName);
                 m_indexDefs.Add(key, tableDef);
             }
 
@@ -375,15 +361,16 @@ namespace EseView
 
         public IEnumerable<DBRow> GetIndexInfo(string tableName, string indexName)
         {
-            List<ColSpec> tableDef = GetIndexTableDef(tableName, indexName);
+            IEnumerable<ColSpec> tableDef = GetIndexTableDef(tableName, indexName);
 
             JET_INDEXLIST indexList;
             Esent.JetGetIndexInfo(m_sesid, m_dbid, tableName, indexName, out indexList, JET_IdxInfo.List);
 
             var columnsByName = new Dictionary<string, int>();
-            for (int i = 0, n = tableDef.Count; i < n; i++)
+            int i = 0;
+            foreach (ColSpec column in tableDef)
             {
-                columnsByName.Add(tableDef[i].Name, i);
+                columnsByName.Add(column.Name, i++);
             }
 
             Esent.JetMove(m_sesid, indexList.tableid, JET_Move.First, MoveGrbit.None);
@@ -394,7 +381,7 @@ namespace EseView
 
                 foreach (ColSpec column in tableDef)
                 {
-                    row.Add(column.Fetch(m_sesid, indexList.tableid, column.ColumnId));
+                    row.Add(column.Retriever(m_sesid, indexList.tableid, column.ColumnId));
                 }
 
                 yield return new DBRow(columnsByName, row);
